71cd10a3eebae6be1c9802d7757cf9909108132a
Junior Rodrigo Oliden <46029524+juniorodrigo@users.noreply.github.com>
2025-08-15 01:31:52 -0500

    feat: mejorar el manejo de errores en el middleware de Express

--- src/config/errors/express-handler.ts
+++ src/config/errors/express-handler.ts
@@ -1,23 +1,21 @@
 import { type Request, type Response, type NextFunction } from 'express';
+import logger from '../logger.js';
 
 function errorHandler(err: Error, req: Request, res: Response, next: NextFunction): any {
-	if (res.headersSent) return next(err);
+	if (res.headersSent) {
+		logger.error(err.message, { stack: err.stack, method: req.method, url: req.originalUrl, headersSent: true });
+		return next(err);
+	}
 
-	// Si es un error operacional, lo manejamos
 	if (err.isOperational) {
-		// logger.error(` ${req.method} ${req.path} | ${err.message}`, {
-		// 	error: err.message,
-		// 	stack: err.stack,
-		// });
+		logger.warn(err.message, { stack: err.stack, method: req.method, url: req.originalUrl });
 		return res.status(err.statusCode ?? 400).json({
 			success: false,
 			message: err.message,
 		});
 	}
 
-	// logger.error(` ${req.method} ____ ${req.url}`, { error: err.message, stack: err.stack });
-
-	// Si no es un error operacional, lo logueamos y respondemos con un error genérico
+	logger.error(err.message, { stack: err.stack, method: req.method, url: req.originalUrl });
 	return res.status(500).json({
 		success: false,
 		message: 'Ha ocurrido un error interno. Intenta más tarde.',
